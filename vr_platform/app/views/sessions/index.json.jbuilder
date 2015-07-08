5681a7af6519d55c4e38f08747effeaef68c7dc4
T Humphrey <thumphrey3@users.noreply.github.com>
2015-07-07 19:07:43 -0700

    Session edits

--- vr_platform/app/views/sessions/index.json.jbuilder
+++ vr_platform/app/views/sessions/index.json.jbuilder
@@ -1,6 +1,6 @@
 json.array! @sessions do |event|
   json.extract! event, :id, :patient_id, :appointment_date, :number
   json.title event.session_label
-  json.start event.created_at
+  json.start event.appointment_date
   json.url session_path(event.id)
 end
--- vr_platform/db/seeds.rb
+++ vr_platform/db/seeds.rb
@@ -6,14 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Patient.delete_all
+Blsexposure.delete_all
+Cycle.delete_all
+Determinant.delete_all
+Session.delete_all
+
 patient_list = [
-  ["Monisha", "Sullivan", "Female", 16, "94121", "Penicillin for her strep throat, ProAir for her asthma, and permethrin for her scabies", ["Asthma", "Lung problems / cough","Depression / Anxiety",""], "(415) 333-2323", "Dr. Nadine Burke Harris", "(650) 309-2013"]
+  ["Monisha", "Sullivan", "Female", 16, "94121", "Penicillin for her strep throat, ProAir for her asthma, and permethrin for her scabies", ["Asthma", "Lung problems / cough","Depression / Anxiety",""], "(415) 333-2323", "Dr. Nadine Burke Harris", "(650) 309-2013"],
+  
   ["Murray", "Barr", "Male", 54, "94403", "Acamprosate, disulfiram", ["Liver problems / Hepatitis", "Psychiatric Care", "Lung problems / cough", ""], "(650) 932-2930", "Marla Johns", "(650) 232-2302"], 
+
   ["Radric", "Davis", "Male", 35, "30316", "Adderall, Hydrochlorothiazide", ["High Blood Pressure", "Headaches / Migraines", "Psychiatric Care", ""], "(404) 121-1017", "Kenneth Bailey", "(404) 232-1017"], 
-  ["Hank", "Hendricks", "Male", 47, "94403", "Oral diabetes medications(ODMs), aspirin, salsalate", ["Diabetes", "Heart Disease / Murmur / Angina", "Depression / Anxiety", ""], "(650) 322-2323", "Jeffrey Brenner", "(650) 923-2039"], 
+
+  ["Hank", "Hendricks", "Male", 47, "94403", "Oral diabetes medications(ODMs), aspirin, salsalate", ["Diabetes", "Heart Disease / Murmur / Angina", "Depression / Anxiety", ""], "(650) 322-2323", "Jeffrey Brenner", "(650) 923-2039"],
+
   ["Vibha", "Gandhi", "Female", 57, "94121", "Pioglitazone, glibenclamide, U-100 insulin", ["Diabetes", "Heart Disease / Murmur / Angina", "Stroke", "Swollen ankles", ""], "(415) 932-9201", "Jayshree Ji", "(415) 331-9203"]
 ]
 
 patient_list.each do |first_name, last_name, gender, age, zipcode, medication, conditions, telephone_no, emergencycontact, emergencycontact_no|
   Patient.create(first_name: first_name, last_name: last_name, gender: gender, age: age, zipcode: zipcode, medication: medication, conditions: conditions, telephone_no: telephone_no, emergencycontact: emergencycontact, emergencycontact_no: emergencycontact_no)
 end
+
+puts "Success: Patients data loaded"
